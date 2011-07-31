8c51d75ef3a8a7ad3be1b439629559d027a1ab26
Sofian Audry <info@sofianaudry.com>
2011-07-31 01:12:57 -0400

    Sync plot_tag.py

--- workshop1.5/plot_tag.py
+++ workshop1.5/plot_tag.py
@@ -12,46 +12,50 @@ def mag(x):
     return sqrt(sum((x*x).transpose()))
 
 if __name__=='__main__':
-    if (len(sys.argv) != 3):
-      print "Usage: " + sys.argv[0] + " <uid> <tagname>"
-      exit(0)
-    
-    if (sys.argv[1] == "all"):
-      uids = [1,2,3,5,7,8,9,10]
-    else:
-      uids = sys.argv[1].split(",")
-    tag = sys.argv[2]
 
-    videoId = -1546120540
-    
-    tags = read_tags()
-    data, fields = read_minibees()
-    
-    tagged = []
-    notTagged = []
-    
-    for uid in uids:
-      uid = int(uid)
-      individual = data[uid]
-      for point in individual:
-        if (point[3] != videoId):
-          continue
-        x = mag(point[5:8])
-        if tags.frame_is(videoId, point[4], tag):
-          tagged.append([point[4], x])
-        else:
-          notTagged.append([point[4], x])
-    
-    clf()
-    if (tagged != []):
-      tagged = array(tagged)
-      plot(tagged[:,0], tagged[:,1], "b.", label=tag)
-    if (notTagged != []):
-      notTagged = array(notTagged)
-      plot(notTagged[:,0], notTagged[:,1], "r.", label="not " + tag)
-    title("Tag \"" + tag + "\" for individuals " + sys.argv[1])
-    xlabel("frame")
-    ylabel("magnitude")
-    legend(loc="upper center")
-    savefig("plots/plot_tag--mag--" + tag + "_" + sys.argv[1].replace(",","-") + ".png")
-    #show()
+  # Init
+  if (len(sys.argv) != 3):
+    print "Usage: " + sys.argv[0] + " <uid> <tagname>"
+    exit(0)
+
+  if (sys.argv[1] == "all"):
+    uids = [1,2,3,5,7,8,9,10]
+  else:
+    uids = sys.argv[1].split(",")
+  tag = sys.argv[2]
+
+  videoId = -1546120540
+  
+  # Read data
+  tags = read_tags()
+  data, fields = read_minibees()
+
+  tagged = []
+  notTagged = []
+
+  for uid in uids:
+    uid = int(uid)
+    individual = data[uid]
+    for point in individual:
+      if (point[3] != videoId):
+        continue
+      x = mag(point[5:8])
+      if tags.frame_is(videoId, point[4], tag):
+        tagged.append([point[4], x])
+      else:
+        notTagged.append([point[4], x])
+
+  # Draw plot
+  clf()
+  if (tagged != []):
+    tagged = array(tagged)
+    plot(tagged[:,0], tagged[:,1], "b.", label=tag)
+  if (notTagged != []):
+    notTagged = array(notTagged)
+    plot(notTagged[:,0], notTagged[:,1], "r.", label="not " + tag)
+  title("Tag \"" + tag + "\" for individuals " + sys.argv[1])
+  xlabel("frame")
+  ylabel("magnitude")
+  legend(loc="upper center")
+  savefig("plots/plot_tag--mag--" + tag + "_" + sys.argv[1].replace(",","-") + ".png")
+  #show()
