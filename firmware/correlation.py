f001b61d6173ac8ca3f4e17b7c557ce5ff15bfa3
Stephen Sinclair <sinclair@music.mcgill.ca>
2011-11-12 13:55:22 -0500

    Use a 3rd-order fit to map frequency to decay coefficient.

--- firmware/correlation.py
+++ firmware/correlation.py
@@ -3,12 +3,16 @@
 from pylab import *
 from math import pi
 
-def inst_covariance(eta):
+# This is a 3rd-order fit to the eta/radial frequency relationship
+# defined by "omega = arccos(1-((a-1)**2)/(2*a))"
+wToEta=array([-0.01183542, -0.12620551,  1.07008204, -0.00857713])
+
+def inst_covariance(w):
+    eta = w*w*w*wToEta[0] + w*w*wToEta[1] + w*wToEta[2] + wToEta[3]
     if eta > 1.762:
         raise Exception('eta not defined for values > 1.762 (due to arccos)')
     a = exp(-eta)
     w = arccos(1-((a-1)**2)/(2*a))
-    print w
     x, y = yield
     s = 0
     while True:
@@ -16,12 +20,12 @@ def inst_covariance(eta):
         s = a*s+(1-a)*r
         x, y = yield s
 
-def inst_correlation(eta):
-    cov1 = inst_covariance(eta)
+def inst_correlation(w):
+    cov1 = inst_covariance(w)
     cov1.next()
-    cov2 = inst_covariance(eta)
+    cov2 = inst_covariance(w)
     cov2.next()
-    cov3 = inst_covariance(eta)
+    cov3 = inst_covariance(w)
     cov3.next()
     cor = 0
     while True:
@@ -38,12 +42,13 @@ def gencos(phase,inc):
         if phase > 2*pi: phase -= 2*pi
 
 def run():
-    tm = arange(0,1,0.001)
+    sr = 1000.0
+    tm = arange(0,1,1/sr)
     c1 = gencos(0,0.025)
     c2 = gencos(pi/2,0.025)
-    cov = inst_covariance(.5)
+    cov = inst_covariance(50/sr*pi)
     cov.next()
-    cor = inst_correlation(.5)
+    cor = inst_correlation(50/sr*pi)
     cor.next()
     yc1 = zeros(len(tm))
     yc2 = zeros(len(tm))
